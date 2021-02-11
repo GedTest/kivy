c5eebfff9244dcc4940465ad666263f4576e6f35
Viktor Zwinger <45140691+GedTest@users.noreply.github.com>
2021-02-11 10:25:17 +0100

    Kivy Tutorial #8 - Simple drawing apps

--- kivyApp.py
+++ kivyApp.py
@@ -1,22 +1,26 @@
 import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import ObjectProperty
+from kivy.graphics import Rectangle
+from kivy.graphics import Color
+from kivy.graphics import Line
 
 
 class Touch(Widget):
-    btn = ObjectProperty(None)
+    def __init__(self, **kwargs):
+        super(Touch, self).__init__(**kwargs)
+
+        with self.canvas:
+            Color(0, 0, 1, 1, mode="rgba")
+            Line(points=(20, 30, 400, 500, 600, 85))
+            Color(1, 0, 0, 0.5, mode="rgba")
+            self.rect = Rectangle(pos=(0, 0), size=(50, 50))
 
     def on_touch_down(self, touch):
-        print("Mouse Down ", touch)
-        self.btn.opacity = 0.5
+        self.rect.pos = touch.pos
 
     def on_touch_move(self, touch):
-        print("Mouse Move ", touch)
-
-    def on_touch_up(self, touch):
-        print("Mouse Up ", touch)
-        self.btn.opacity = 1.0
+        self.rect.pos = touch.pos
 
 
 class MyApp(App):
