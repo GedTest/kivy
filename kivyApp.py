40afde33450ca55656077327e800a60a03f2f29a
Viktor Zwinger <45140691+GedTest@users.noreply.github.com>
2021-02-11 09:31:26 +0100

    Kivy Tutorial #2 - Labels, Input and GUI layouts

--- kivyApp.py
+++ kivyApp.py
@@ -1,11 +1,30 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+
+class Grid(GridLayout):
+    def __init__(self, **kwargs):
+        super(Grid, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text="First name:"))
+        self.first_name = TextInput(multiline=False)
+        self.add_widget(self.first_name)
+
+        self.add_widget(Label(text="Last name:"))
+        self.last_name = TextInput(multiline=False)
+        self.add_widget(self.last_name)
+
+        self.add_widget(Label(text="Email:"))
+        self.email = TextInput(multiline=False)
+        self.add_widget(self.email)
 
 
 class MyApp(App):
     def build(self):
-        return Label(text="Hello world")
+        return Grid()
 
 
 if __name__ == "__main__":
